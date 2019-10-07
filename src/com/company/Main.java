8b84412da69996f208ce60155b3ce4e9d981766f
HariP5 <hari.pokhrel.2020@student.lhschools.org>
2019-10-06 23:36:13 -0400

    Added scanner keyboard

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         //todo declare variable
-
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
 
 
         //todo initialize
