d1ac123905d2490c5e21fe0d9867af0e4e3bdc95
HariP5 <hari.pokhrel.2020@student.lhschools.org>
2019-10-06 23:39:17 -0400

    Added conditionals

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,13 +20,37 @@ public class Main {
         System.out.println("Give me a number");
         number = keyboard.nextDouble();
 
-
-
         //todo conditional
-
-
-
-
+        if (10>=number && number>0) {
+            System.out.println("Most likely not");
+        }
+        else if (20>=number & number>10) {
+            System.out.println("Most likely");
+        }
+        else if (30>=number & number>20) {
+            System.out.println("Likely");
+        }
+        else if (40>=number & number>30) {
+            System.out.println("NOT likely");
+        }
+        else if (50>=number & number>40) {
+            System.out.println("Yes");
+        }
+        else if (60>=number & number>50) {
+            System.out.println("Probably");
+        }
+        else if (70>=number & number>60) {
+            System.out.println("Probably Not");
+        }
+        else if (80>=number & number>70) {
+            System.out.println("Probably YES");
+        }
+        else if (90>=number & number>80) {
+            System.out.println("You might");
+        }
+        else if (100>=number & number>90) {
+            System.out.println("You might NOT");
+        }
 
     }
 }
